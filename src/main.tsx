d977ae68f6ea581353ecb7e89e59abfd1511d20c
sam <savantsamarth@gmail.com>
2025-01-16 16:27:24 +0530

    fix- match react router root url with deployed url

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById("root")!).render(
       <BrowserRouter>
         <Routes>
           <Route
-            path="/"
+            path="/mart-ecommerce-app-react/"
             element={<App />}
           >
             <Route
